2e12d6cb87b862e72a33d655f3ad78ad3c05ef07
Lucas Delobelle <lucas@ld-web.net>
2023-09-05 16:26:48 +0200

    :sparkles: add insert product endpoint

---
+++ 02-products-api-server/Response.php
@@ -0,0 +1,8 @@
+<?php
+
+class Response
+{
+  public const OK = 200;
+  public const CREATED = 201;
+  public const INTERNAL_SERVER_ERROR = 500;
+}
--- 02-products-api-server/insert-product.php
+++ 02-products-api-server/insert-product.php
@@ -1,12 +1,44 @@
 <?php
 
 require_once 'functions.php';
+require_once 'Response.php';
+
+set_exception_handler(function (Throwable $ex): void {
+  http_response_code(Response::INTERNAL_SERVER_ERROR);
+  echo json_encode([
+    'error' => [
+      'code' => $ex->getCode(),
+      'message' => "Une erreur est survenue"
+    ]
+  ]);
+});
 
 $pdo = getConnection();
 
 header('Content-type: application/json; charset=UTF-8');
 
+// J'extraie les données du corps de la requête
 $json = file_get_contents('php://input');
+// Je décode les données extraites
+// De JSON à un tableau associatif PHP
 $data = json_decode($json, true);
 
-var_dump($data);
+// $data est à présent un tableau associatif
+// indexé sur les propriétés de l'objet JSON transmis
+
+// Je vais donc utiliser les valeurs qui se trouvent derrière
+// les clés "name" et "price"
+// pour préparer un statement INSERT, puis l'exécuter avec ces valeurs
+$insertStmt = $pdo->prepare("INSERT INTO products (name, price) VALUES (?, ?)");
+$insertStmt->execute([$data['name'], $data['price']]);
+
+// Rechercher un code de réponse HTTP adapté en cas de succès de création
+http_response_code(Response::CREATED);
+
+// Puis, dans le corps de la réponse, renvoyer le produit créé, avec son ID
+$productId = $pdo->lastInsertId();
+
+echo json_encode([
+  'id' => intval($productId),
+  ...$data
+]);
